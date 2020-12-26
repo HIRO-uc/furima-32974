2e5049787d2e4379f32bcfce0f463dd9a5d9fdd4
HIRO-uc <hiro.nyanta.mech@gmail.com>
2020-12-26 21:46:13 +0900

    purchase_addressモデルのダミーデータの追加

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    postal_code { '123-4567'}
+    shipping_prefecture_id { 1 } 
+    city { '新宿区' }
+    house_number { '新町1-1-1' }
+    building_name { 'コーポ新宿101' }
+    tel { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
+    item_id { 1 }
+    user_id { 1 }
   end
 end
