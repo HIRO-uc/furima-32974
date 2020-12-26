1d9b5e72f50b8575c40bd7f5a8142d2d2d338b10
HIRO-uc <hiro.nyanta.mech@gmail.com>
2020-12-26 14:54:06 +0900

    purchaseコントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,33 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_address_params
+    params.permit(:postal_code, :shipping_prefecture_id, :city, :house_number, :building_name, :tel, :token, :item_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 end
