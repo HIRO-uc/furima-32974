189df7c619db08e2e84a7c510918a4784e9d11ba
HIRO-uc <hiro.nyanta.mech@gmail.com>
2020-12-26 14:53:46 +0900

    formオブジェクトパターン実装のためのモデル追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,4 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_prefecture
-
-  validates :postal_code, :city, :house_number, :tel, presence: true
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
-  validates :tel,         format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
-  validates :shipping_prefecture_id, numericality: { other_than: 0, message: 'Select' }
 end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :shipping_prefecture_id, :city, :house_number, :building_name, :tel, :token, :item_id, :user_id
+
+  validates :token,                      presence: true
+  with_options presence: true do
+    validates :postal_code,                format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+  end
+  validates :shipping_prefecture_id, numericality: { other_than: 0, message: 'Select' }
+  validates :city, :house_number,        presence: true
+  with_options presence: true do
+    validates :tel,                        format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+  end
+  
+  def save
+    Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, shipping_prefecture_id: shipping_prefecture_id, city: city, house_number: house_number, building_name: building_name, tel: tel)
+  end
+end
