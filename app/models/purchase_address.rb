dd457ab63dfbdc3522e9b0a1f5135282b3595d3e
HIRO-uc <hiro.nyanta.mech@gmail.com>
2020-12-27 12:47:13 +0900

    Rubocopによる修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,14 +6,14 @@ class PurchaseAddress
     validates :token
     validates :user_id
     validates :item_id
-    validates :postal_code,                format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
     validates :city
     validates :house_number
-    validates :tel,                        format: { with: /\A\d{10,11}\z/, message: 'Input only half-width number' }
+    validates :tel, format: { with: /\A\d{10,11}\z/, message: 'Input only half-width number' }
   end
-  
+
   validates :shipping_prefecture_id, numericality: { other_than: 0, message: 'Select' }
- 
+
   def save
     Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, shipping_prefecture_id: shipping_prefecture_id, city: city,
