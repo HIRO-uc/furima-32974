9600e891c676b7275266e03b0b19bfdeba68b0f5
HIRO-uc <hiro.nyanta.mech@gmail.com>
2020-12-27 12:16:19 +0900

    formオブジェクトモデルのバリデーション修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,16 +2,18 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_prefecture_id, :city, :house_number, :building_name, :tel, :token, :item_id, :user_id
 
-  validates :token, presence: true
   with_options presence: true do
+    validates :token
+    validates :user_id
+    validates :item_id
     validates :postal_code,                format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
-  end
-  validates :shipping_prefecture_id, numericality: { other_than: 0, message: 'Select' }
-  validates :city, :house_number, presence: true
-  with_options presence: true do
+    validates :city
+    validates :house_number
     validates :tel,                        format: { with: /\A\d{10,11}\z/, message: 'Input only half-width number' }
   end
-
+  
+  validates :shipping_prefecture_id, numericality: { other_than: 0, message: 'Select' }
+ 
   def save
     Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, shipping_prefecture_id: shipping_prefecture_id, city: city,
